8823cd1316b278ea1ac081d95f66f4934e1278da
jerryjiang <jerryjiang.jc@gmail.com>
2024-09-18 18:44:13 +0800

    init mora

---
+++ commonsense_reasoning/train.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+rank=32
+alpha=64
+gpuid=3
+
+model_p_or_n=yahma/llama-7b-hf
+
+model_path=trained_models/mlora-r$rank-a$alpha-3e4
+results_path=results/mlora-r$rank-a$alpha-3e4
+
+mkdir -p $model_path
+mkdir -p $results_path
+
+# MLoRA: --use_lora_router
+# MixLoRA: --use_lora_router --use_lora_router_mixer
+
+CUDA_VISIBLE_DEVICES=$gpuid python -u finetune.py \
+  --base_model $model_p_or_n \
+  --data_path 'ft_training_set/commonsense_170k.json' \
+  --output_dir $model_path \
+  --batch_size 16 \
+  --micro_batch_size 4 \
+  --num_epochs 3 \
+  --learning_rate 3e-4 \
+  --cutoff_len 256 \
+  --val_set_size 120 \
+  --adapter_name lora \
+  --lora_r $rank \
+  --lora_alpha $alpha \
+  --use_lora_router \
+  --target_modules "["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]"
+
+
+for ds in ARC-Easy openbookqa social_i_qa ARC-Challenge winogrande piqa boolq hellaswag
+do
+  CUDA_VISIBLE_DEVICES=$gpuid python -u commonsense_evaluate.py \
+    --model LLaMA \
+    --adapter LoRA \
+    --dataset $ds \
+    --batch_size 1 \
+    --base_model $model_p_or_n \
+    --lora_weights $model_path \
+    --save_dir $results_path
+done
